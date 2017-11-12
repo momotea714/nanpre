0a5618eecfca91c3418dc30b57ab72b948628dca
karimata1106 <karimata1106@gmail.com>
2017-11-12 22:52:28 +0900

    セル選択時の処理がコントローラー経由になってしまっている #3

--- Sudoku/Scripts/common.js
+++ Sudoku/Scripts/common.js
@@ -3,37 +3,31 @@
     var selectId = "0";
     var connection = $.hubConnection();
     var echo = connection.createHubProxy("myHub");
-    
-    echo.on("SelectTd", function (id) {
-        $(".trout").each(function(index, element) {
-            $(element).css("color", "black");
-            $(element).css("background-color", "");
-        });
-        $("#" + id).css("color", "white");
-        $("#" + id).css("background-color", "#2d89ef");
-    });
+
     echo.on("InputNumber", function (id, number) {
         $("#" + id).text(number);
     });
-    //echo.on("NextQuestion", function () {
-    //    location.reload();
-    //});
 
-
+    //数独のマス選択時のイベントハンドラ
     $(".target_table td").on("click", function (e) {
         // currentTarget のidを取得
         selectId = $(e.currentTarget).attr("id");
-        echo.invoke("SelectTd", selectId);
+
+        //数独のマス全てに背景色と文字色をデフォルトに設定しなおす
+        $(".trout").each(function (index, element) {
+            $(element).css("color", "black");
+            $(element).css("background-color", "");
+        });
+
+        //選択しているマスのみ背景色と文字色を選択色に変更する
+        $("#" + selectId).css("color", "white");
+        $("#" + selectId).css("background-color", "#2d89ef");
     });
 
     $(".sudokuNumber").on("click", function (e) {
         var btnText = $(e.currentTarget).text();
         echo.invoke("InputNumber", selectId, btnText,groupId);
-    }); 
-
-    //$("#nextQuestion").on("click", function (e) {
-    //    echo.invoke("NextQuestion");
-    //});
+    });
 
     connection.start(function () {
         echo.invoke("Join", $('#RommID').val());
