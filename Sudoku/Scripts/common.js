3d1fb87a160bc1d26650237f9700c7bc416f2aae
karimata1106 <karimata1106@gmail.com>
2017-11-20 01:10:07 +0900

    固定値セル選択時にも背景色を入力可能セルと同様にしたい #6

--- Sudoku/Scripts/common.js
+++ Sudoku/Scripts/common.js
@@ -2,6 +2,7 @@
 
     var preSelectTroutId = "0";
     var currentSelectTroutId = "0";
+    var possibleInputTrout = true;
 
     //サーバとの接続オブジェクト作成
     var connection = $.hubConnection();
@@ -11,25 +12,42 @@
 
     //サーバから呼び出される関数を登録
     echo.on("InputNumber", function (id, number) {
-        $("#" + id).text(number);
+        $(id).text(number);
     });
 
     //数独のマス選択時のイベントハンドラ
     $(".trout").on("click", function (e) {
         //選択しているマスのidを取得
         preSelectTroutId = currentSelectTroutId;
-        currentSelectTroutId = $(e.currentTarget).attr("id");
+        currentSelectTroutId = "#" + $(e.currentTarget).attr("id");
 
         //前回選択していたマスの背景色と文字色をデフォルトに設定しなおす
-        $("#" + preSelectTroutId).removeClass('cellClicked');
+        $(preSelectTroutId).removeClass('cellClicked');
 
         //選択しているマスの背景色と文字色を選択色に変更する
-        $("#" + currentSelectTroutId).addClass('cellClicked');
+        $(currentSelectTroutId).addClass('cellClicked');
+
+        //classの値を取得
+        var classVal = $(currentSelectTroutId).attr('class');
+
+        //取得したclassを分割
+        var classValues = classVal.split(' ');
+
+        //取得したclass内にfixがあれば数字を変更できないように変更する
+        if ($.inArray("fix", classValues) >= 0) {
+            possibleInputTrout = false;
+        } else {
+            possibleInputTrout = true;
+        }
     });
 
+    //ナンプレに入力する数字クリック時のイベントハンドラ
     $(".sudokuNumber").on("click", function (e) {
-        var btnText = $(e.currentTarget).text();
-        echo.invoke("InputNumber", currentSelectTroutId, btnText, "nngo" + $("#RoomID").val());
+        //入力可能セル選択時にのみセルの値を書き換える
+        if (possibleInputTrout) {
+            var btnText = $(e.currentTarget).text();
+            echo.invoke("InputNumber", currentSelectTroutId, btnText, "nngo" + $("#RoomID").val());
+        }
     });
 
     //接続を開始
